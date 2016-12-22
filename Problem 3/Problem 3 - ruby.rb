02ed4ca07770a086d068bd304e3bc9bd36174d1f
Valanidas <john.valanidas@gmail.com>
2016-12-21 22:03:35 -0500

    Completion of problem 3

--- Problem 3/Problem 3 - ruby.rb
+++ Problem 3/Problem 3 - ruby.rb
@@ -2,16 +2,41 @@
 
 
 def largest_prime_multiple num
-biggist
-num = num/2
-while num > 0
-  if Prime.instance.prime?(num)
-    
+  prime_array = []
+  prime = 2;
+  while num > 1
+    if num%prime == 0
+      prime_array.push(prime)
+      num = num/prime
+    end
+    prime = next_prime(prime)
   end
-  num = num -2
+  prime_array
 end
 
 
-def prime_multiple_check (multiple, multiple_of)
-
+def next_prime prime
+  not_found = true
+  while not_found
+    prime = prime + 1
+    if prime_checker(prime)
+      not_found = false
+    end
+  end
+  prime
 end
+
+def prime_checker num
+  divsor = 2
+  while (divsor < num/2)
+    if num%divsor == 0
+      return false
+    end
+    divsor = divsor + 1
+  end
+  return true
+end
+
+
+
+puts largest_prime_multiple(600851475143)
